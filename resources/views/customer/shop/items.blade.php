63445227f81e647e347ee4fae0839b7445901ecd
mykill69 <baliviamichael@gmail.com>
2026-06-05 23:39:52 +0800

    Update

--- resources/views/customer/shop/items.blade.php
+++ resources/views/customer/shop/items.blade.php
@@ -445,9 +445,9 @@
             left: 0;
             right: 0;
             bottom: 0;
-            /* background: rgba(0, 0, 0, 0.4) !important;
+            background: rgba(0, 0, 0, 0.4) !important;
             backdrop-filter: blur(4px);
-            -webkit-backdrop-filter: blur(4px); */
+            -webkit-backdrop-filter: blur(4px);
             /* transition: all 0.3s ease; */
             z-index: 9999;
             display: none;
